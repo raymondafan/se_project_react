c5558df978f73fd2310864a8b8c69077333692f3
RaymondAfan <raymond.afan@icloud.com>
2023-12-20 19:05:06 -0800

    ModalWithForm class and styles

--- src/App/App.js
+++ src/App/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Header from "../Header/Header";
 import Main from "../Main/Main";
 import Footer from "../Footer/Footer";
+import ModalWithForm from "../ModalWithForm/ModalWithForm";
 function App() {
   const weatherTemp = "-102°F";
   return (
@@ -9,6 +10,9 @@ function App() {
       <Header />
       <Main weatherTemp={weatherTemp} />
       <Footer/>
+      <ModalWithForm title="New Garment">
+        These are the children
+        </ModalWithForm>
     </div>
   );
 }
