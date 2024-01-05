1f977bb74329015cea5af9215fab1bdae391e20e
RaymondAfan <raymond.afan@icloud.com>
2024-01-04 17:55:26 -0800

    utils

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,4 +1,4 @@
-import { defaultClothingItems } from "../util/constants";
+import { defaultClothingItems } from "../utils/constants";
 import WeatherCard from "../WeatherCard/WeatherCard";
 import ItemCard from "../ItemCard/ItemCard";
 import { useMemo } from "react";
