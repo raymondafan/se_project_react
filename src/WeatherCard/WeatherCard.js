32b40b7ec84180ce2fe1eb4f913a0be897b79920
RaymondAfan <raymond.afan@icloud.com>
2024-01-04 18:09:58 -0800

    organizing constants

--- src/WeatherCard/WeatherCard.js
+++ src/WeatherCard/WeatherCard.js
@@ -1,26 +1,20 @@
 import "./WeatherCard.css";
-const weatherOptions= [
-    {url: require("../images/day/sunny.svg").default, day: true, type: "sunny"},
-    {url: require("../images/day/cloudy.svg").default, day: true, type: "cloudy"},
-    {url: require("../images/night/moon.svg").default, day: false, type: "moon"},
-    {url: require("../images/night/cloud.svg").default, day: false, type: "cloud"}
-    
-];
+import { weatherOptions } from "../utils/constants";
 
-const WeatherCard = ({day, type, weatherTemp= ""}) => {
-    console.log("weather card");
-    const imageSrc= weatherOptions.filter((i)=>{
-        // console.log(i);
-        return i.day === day && i.type === type;
-    });
-    // console.log(imageSrc);
-    // console.log(imageSrc[0].url);
-    const imageSrcUrl = imageSrc[0].url || "";
-    return (
-        <section className="weather" id="weather">
-        <div className="weather_info">{weatherTemp}°F </div>
-          <img src={imageSrcUrl} className="weather_image"/>
-      </section>
-    )
+const WeatherCard = ({ day, type, weatherTemp = "" }) => {
+  console.log("weather card");
+  const imageSrc = weatherOptions.filter((i) => {
+    // console.log(i);
+    return i.day === day && i.type === type;
+  });
+  // console.log(imageSrc);
+  // console.log(imageSrc[0].url);
+  const imageSrcUrl = imageSrc[0].url || "";
+  return (
+    <section className="weather" id="weather">
+      <div className="weather_info">{weatherTemp}°F </div>
+      <img src={imageSrcUrl} className="weather_image" />
+    </section>
+  );
 };
 export default WeatherCard;
