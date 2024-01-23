2f04f8b4f4b3f81972389d86ac44ceab0aa120e9
RaymondAfan <raymond.afan@icloud.com>
2024-01-22 20:27:16 -0800

    update ui/ux profile

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -5,12 +5,12 @@ import ClothesSection from "./ClothesSection";
 const Profile = ({clothingItems, onSelectCard, onCreateModal}) => {
   
     return (
-    <div className="main">    
+    
     <div className="profile">
       <div className="profile__sideBar"><SideBar/></div>
       <div className="profile__items"><ClothesSection clothingItems={clothingItems} onSelectCard={onSelectCard} onCreateModal={onCreateModal}/></div>
     </div>
-   </div>
+  
   );
 };
 export default Profile;
