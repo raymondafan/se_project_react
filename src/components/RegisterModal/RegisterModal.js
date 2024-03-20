871b8f28cfd8a27985025f24384b033424c7d713
RaymondAfan <raymond.afan@icloud.com>
2024-03-19 21:34:19 -0700

    refactor

--- src/components/RegisterModal/RegisterModal.js
+++ src/components/RegisterModal/RegisterModal.js
@@ -6,68 +6,68 @@ const RegisterModal = ({ isOpen, onClose, handleRegister }) => {
   const [password, setPassword] = useState("");
   const [name, setName] = useState("");
   const [avatar, setAvatar] = useState("");
-};
 
-useEffect(() => {
-  if (isOpen) {
-    setEmail("");
-    setPassword("");
-    setName("");
-    setAvatar("");
-  }
-}, [isOpen]);
+  useEffect(() => {
+    if (isOpen) {
+      setEmail("");
+      setPassword("");
+      setName("");
+      setAvatar("");
+    }
+  }, [isOpen]);
 
-const handleEmail = (e) => {
-  setEmail(e.target.value);
-};
-const handlePassword = (e) => {
-  setPassword(e.target.value);
-};
-const handleName = (e) => {
-  setName(e.target.value);
-};
-const handleAvatar = (e) => {
-  setAvatar(e.target.value);
-};
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+  };
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+  };
+  const handleName = (e) => {
+    setName(e.target.value);
+  };
+  const handleAvatar = (e) => {
+    setAvatar(e.target.value);
+  };
 
-const handleSubmit = (e) => {
-  e.preventDefault();
-  handleRegister({ email, password, name, avatar });
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    handleRegister({ email, password, name, avatar });
+  };
+  return (
+    <ModalWithForm
+      onSubmit={handleSubmit}
+      onClose={onClose}
+      name="signup"
+      title="Register here"
+      buttonText="Sign Up"
+      isOpen={isOpen}
+    >
+      <label className="modal__label">
+        Name
+        <input
+          className="modal__input"
+          type="text"
+          placeholder="name"
+          name="name"
+          value={name}
+          onChange={handleNameChange}
+          minLength="1"
+          maxLength="30"
+        />
+      </label>
+      <label className="modal__label">
+        Name
+        <input
+          className="modal__input"
+          type="text"
+          placeholder="name"
+          name="name"
+          value={name}
+          onChange={handleNameChange}
+          minLength="1"
+          maxLength="30"
+        />
+      </label>
+    </ModalWithForm>
+  );
 };
-return (
-  <ModalWithForm
-    onSubmit={handleSubmit}
-    onClose={onClose}
-    name="signup"
-    title="Register here"
-    buttonText="Sign Up"
-    isOpen={isOpen}
-  >
-    <label className="modal__label">
-      Name
-      <input
-        className="modal__input"
-        type="text"
-        placeholder="name"
-        name="name"
-        value={name}
-        onChange={handleNameChange}
-        minLength="1"
-        maxLength="30"
-      />
-    </label>
-    <label className="modal__label">
-      Name
-      <input
-        className="modal__input"
-        type="text"
-        placeholder="name"
-        name="name"
-        value={name}
-        onChange={handleNameChange}
-        minLength="1"
-        maxLength="30"
-      />
-    </label>
-  </ModalWithForm>
-);
