af0d5ce2590d15d4e26761d886fe656a26ca67e1
RaymondAfan <raymond.afan@icloud.com>
2024-05-24 14:56:37 -0700

    baseurl

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,6 +1,6 @@
 import { handleServerResponse } from "./utils";
 // const baseUrl = "http://localhost:3001";
-const baseUrl = process.env.production
+const baseUrl = process.env.NODE_ENV === "production"
   ? "https://api.raymondafanwtwr.strangled.net"
   : "http://localhost:3001";
 const signUp = ({ name, avatar, email, password }) => {
